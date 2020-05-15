22213fe5dd05e3f9cd782404f5afd7c1ea14f59a
Epimodev <godin.fred8@gmail.com>
2020-05-15 23:40:26 +0200

    feat(blog): write prismic types for blog content

--- src/types/prismic.d.ts
+++ src/types/prismic.d.ts
@@ -103,3 +103,72 @@ interface PrismicSearchResponse<T = any> {
   version: string
   license: string
 }
+
+interface PrismicStrongSpan {
+  type: "strong"
+  start: number
+  end: number
+}
+interface PrismicEmSpan {
+  type: "em"
+  start: number
+  end: number
+}
+interface PrismicLinkSpan {
+  type: "hyperlink"
+  start: number
+  end: number
+  data: {
+    link_type: string
+    url: string
+  }
+}
+type PrismicSpan = PrismicStrongSpan | PrismicEmSpan | PrismicLinkSpan
+
+interface PrismicTextNode {
+  type:
+    | "heading1"
+    | "heading2"
+    | "heading3"
+    | "heading4"
+    | "heading5"
+    | "heading6"
+    | "paragraph"
+    | "preformatted"
+    | "o-list-item"
+    | "list-item"
+  text: string
+  spans: PrismicSpan[]
+}
+
+interface PrismicImageNode {
+  type: "image"
+  url: string
+  alt: string
+  copyright: string | null
+  dimensions: {
+    width: number
+    height: number
+  }
+}
+
+interface PrismicEmbedNode {
+  type: "embed"
+  oembed: {
+    type: "video" | "rich"
+    embed_url: string
+    title: string
+    provider_name: string
+    thumbnail_url: string | null
+    thumbnail_width?: number
+    thumbnail_height?: number
+    height: number | null
+    width: number | null
+    version: string
+    author_name: string
+    author_url: string
+    provider_url: string
+    cache_age: number | null
+    html: string
+  }
+}
