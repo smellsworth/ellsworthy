b69f1954d844ae512b80446968346c14d107650a
Epimodev <godin.fred8@gmail.com>
2020-05-21 19:10:36 +0200

    feat(backend): add polka types for backend routes

--- src/routes/blog/kudos/[slug].json.ts
+++ src/routes/blog/kudos/[slug].json.ts
@@ -1,6 +1,7 @@
+import { Request, Response } from 'polka'
 import { readValue, setValue } from "../../../server-utils/firebase"
 
-export async function get(req: any, res: any) {
+export async function get(req: Request, res: Response) {
   try {
     const count = await readValue<number | null>(
       `/kudos_count/${req.params.slug}`
@@ -10,11 +11,11 @@ export async function get(req: any, res: any) {
   } catch (error) {
     console.error(error)
     // we ignore errors because this feature is just a nice to have
-    res.end("0")
+    res.end(0)
   }
 }
 
-export async function post(req: any, res: any) {
+export async function post(req: Request, res: Response) {
   try {
     const path = `/kudos_count/${req.params.slug}`
     const count = await readValue<number | null>(path)
