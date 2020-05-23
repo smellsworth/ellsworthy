06d7600c37c1fe88f65e64fdd6939f877d359ee2
Epimodev <godin.fred8@gmail.com>
2020-05-23 09:59:39 +0200

    feat(firebase): make firebase easier to setup

--- src/server-utils/firebase.ts
+++ src/server-utils/firebase.ts
@@ -2,22 +2,11 @@ import admin from "firebase-admin"
 import dotenv from "dotenv"
 dotenv.config({ path: '.env.local' })
 
-const serviceAccount = {
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-}
-
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT!)
+serviceAccount.private_key = process.env.FIREBASE_PRIVATE_KEY,
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount as any),
+  credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FIREBASE_URL,
   databaseAuthVariableOverride: {
     uid: "WEBSITE_BACKEND",
