9a6f0dee3471a1a53dc7d466216e8bf5d4bc76be
Epimodev <godin.fred8@gmail.com>
2020-06-22 21:40:23 +0200

    feat(index): create about component

---
+++ src/components/About.svelte
@@ -0,0 +1,26 @@
+<style>
+  .container {
+    display: flex;
+    align-items: center;
+    border: 1px solid var(--border_color);
+  }
+
+  img {
+    width: 202px;
+    margin-right: 24px;
+  }
+
+  p {
+    margin: 12px 0;
+  }
+</style>
+
+<div class="container">
+  <img alt="Success Kid" src="half-matt.png" />
+  <div>
+    <p>Trying to figure out where/what to eat.</p>
+    <p>COO @PandaScore. Food, Esports, Business.</p>
+    <p>Ex-Partner @500startups.</p>
+    <p>Bad opinions are probably mine. 食い倒れ He/him.</p>
+  </div>
+</div>
