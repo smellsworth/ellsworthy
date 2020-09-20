bc34d45ffa186558bac9001a2e82428f853ea839
Epimodev <godin.fred8@gmail.com>
2020-09-20 22:27:45 +0200

    feat(opengraph): add opengraph tags and image

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -18,16 +18,32 @@
 
   export let essays
   export let projects
+
+  const url = "https://matt-website.now.sh/"
+  const imageUrl = `${url}og-image.png`
+  const title = "Ellsworthy 🍕"
+  const description =
+    "Trying to figure out where/what to eat. COO @PandaScore. Food, Esports, Business. Ex-Partner @500startups. Bad opinions are probably mine. 食い倒れ He/him."
 </script>
 
 <svelte:head>
-  <title>Ellsworthy 🍕</title>
-  <meta
-    name="description"
-    content="Trying to figure out where/what to eat. COO @PandaScore. Food,
-    Esports, Business. Ex-Partner @500startups. Bad opinions are probably mine.
-    食い倒れ He/him."
-  />
+  <title>{title}</title>
+  <meta name="title" content="{title}" />
+  <meta name="description" content="{description}" />
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="blog" />
+  <meta property="og:url" content="{url}" />
+  <meta property="og:title" content="{title}" />
+  <meta property="og:description" content="{description}" />
+  <meta property="og:image" content="{imageUrl}" />
+
+  <!-- Twitter -->
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content="{url}" />
+  <meta property="twitter:title" content="{title}" />
+  <meta property="twitter:description" content="{description}" />
+  <meta property="twitter:image" content="{imageUrl}" />
 </svelte:head>
 
 <About />
