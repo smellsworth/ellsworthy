3ae449a96a9d1602e172511cc7c1999689cd100c
Epimodev <godin.fred8@gmail.com>
2020-09-30 21:20:47 +0200

    feat(about): fix unexpected spaces

--- src/components/About.svelte
+++ src/components/About.svelte
@@ -45,23 +45,17 @@
   }
 </style>
 
-<!-- prettier-ignore -->
 <!-- use prettier ignore to avoid line breaks in paragraph which add unexpected spaces -->
+<!-- prettier-ignore -->
 <div class="container">
   <img alt="Matt illustration" src="half-matt.png" />
   <div>
     <p>Trying to figure out where/what to eat.</p>
     <p>
-      COO
-      <a href="https://pandascore.co/" target="_blank" rel="noopener">
-        @PandaScore
-      </a>
-      . Food, Esports, Business.
+      COO <a href="https://pandascore.co/" target="_blank" rel="noopener">@PandaScore</a>. Food, Esports, Business.
     </p>
     <p>
-      Ex-Partner
-      <a href="https://500.co/" target="_blank" rel="noopener">@500startups</a>
-      .
+      Ex-Partner<a href="https://500.co/" target="_blank" rel="noopener">@500startups</a>.
     </p>
     <p>Bad opinions are probably mine. 食い倒れ He/him.</p>
   </div>
