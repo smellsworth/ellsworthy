30378cfc29284d1a7f6f73ffdf1e2956db25d011
Epimodev <godin.fred8@gmail.com>
2020-11-03 16:23:35 +0100

    feat(firebase): add logs in case connexion fail

--- api/_firebase.ts
+++ api/_firebase.ts
@@ -1,17 +1,27 @@
 import admin from "firebase-admin"
 import dotenv from "dotenv"
-dotenv.config({ path: '.env' })
+dotenv.config({ path: ".env" })
 
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT!)
-serviceAccount.private_key = process.env.FIREBASE_PRIVATE_KEY,
+serviceAccount.private_key = process.env.FIREBASE_PRIVATE_KEY
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_URL,
-  databaseAuthVariableOverride: {
-    uid: "WEBSITE_BACKEND",
-  },
-})
+try {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: process.env.FIREBASE_URL,
+    databaseAuthVariableOverride: {
+      uid: "WEBSITE_BACKEND",
+    },
+  })
+} catch (e) {
+  console.log({
+    FIREBASE_URL: process.env.FIREBASE_URL,
+    FIREBASE_SERVICE_ACCOUNT: process.env.FIREBASE_SERVICE_ACCOUNT,
+    FIREBASE_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
+  })
+
+  throw e
+}
 
 function readValue<T>(path: string): Promise<T> {
   return new Promise((resolve, reject) => {
